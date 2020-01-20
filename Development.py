e33c7fb0dd2fb08f5d29d0de6f33e424056aac3e
Arsheya J <a76jain@edu.uwaterloo.ca>
2020-01-20 17:35:24 -0500

    created remove_url function to clean queried data

--- Development.py
+++ Development.py
@@ -26,3 +26,29 @@ access_token_secret = 'vkExH4ol3lMfbUFvqTixA7tCFOHEhSU8CGsoSftCvwLwB'
 auth = tw.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tw.API(auth, wait_on_rate_limit=True)
+
+def remove_url(txt):
+    """Replace URLs found in a text string with nothing 
+    (i.e. it will remove the URL from the string).
+
+    Parameters
+    ----------
+    txt : string
+        A text string that you want to parse and remove urls.
+
+    Returns
+    -------
+    The same txt string with url's removed.
+    """
+
+    return " ".join(re.sub("([^0-9A-Za-z \t])|(\w+:\/\/\S+)", "", txt).split())
+# Create a custom search term and define the number of tweets
+search_term = "#climate+change -filter:retweets"
+
+tweets = tw.Cursor(api.search,
+                   q=search_term,
+                   lang="en",
+                   since='2018-11-01').items(1000)
+
+# Remove URLs
+tweets_no_urls = [remove_url(tweet.text) for tweet in tweets]
