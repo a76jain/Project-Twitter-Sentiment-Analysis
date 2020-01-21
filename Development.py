1f663d5005f34865b3ab9f5ff22897119f6eef78
a76jain <31836942+a76jain@users.noreply.github.com>
2020-01-20 20:17:19 -0500

    Update Development.py

--- Development.py
+++ Development.py
@@ -18,11 +18,6 @@ warnings.filterwarnings("ignore")
 sns.set(font_scale=1.5)
 sns.set_style("whitegrid")
 
-consumer_key = '4MvRITRR9qYgiZyHROEXy8zif'
-consumer_secret = 'q5ZCMqiWL5kF5vklJUNR348tfQhk26Lq7AhVgbGWTATGiFDnGL'
-access_token = '1131361591583281153-HMzJUAKGqFvGeS4H1xAwjzDI7DZhAB'
-access_token_secret = 'vkExH4ol3lMfbUFvqTixA7tCFOHEhSU8CGsoSftCvwLwB'
-
 auth = tw.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tw.API(auth, wait_on_rate_limit=True)
